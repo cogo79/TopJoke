d4bf23120d871441e4db7cfa5a11a51ae9b6bb9e
cogo79 <migobigo76@gmail.com>
2014-10-29 20:29:51 +0100

    Figured out how to add data to Backbone view template.

--- js/app.js
+++ js/app.js
@@ -10,14 +10,39 @@ require(['app/model/Group', 'app/model/Person', 'app/model/Joke', 'app/model/Rat
 		"username" : "jennybenny",
 		"password" : "nej444422",
 	//	"email" : "jenny@aftonbladet.se"
-	});
+});
 	group.persons().add(miguel);
 	group.persons().add(jenny);
 	group2.persons().add(miguel);
 	group2.persons().add(jenny);
 
+	JokeView = Backbone.View.extend({
+		template: _.template($("#joke_template").html()),
+		initialize: function(){
+			this.render();
+		},
+		render: function(){
+            //Pass variables in using Underscore.js Template
+            console.log("So far so good");
+            var variables = { jokeTitle: "hahahahaha ;D", joke: "Group loop croooop hahahah ;D", jokeAuthor: "Miguel Ehrstrand" };
 
+    // Pass this object onto the template function.
+    // This returns an HTML string.
+    var html = this.template(variables);
 
+    // Append the result to the view's element.
+    $(this.el).append(html);
+            /*
+            // Compile the template using underscore
+            var template = _.template( $("#joke_template").html(), variables );
+            // Load the compiled HTML into the Backbone "el"
+            this.$el.html( template );
+            */
+        }
+    });
+
+	var joke_view = new JokeView({ el: $("#joke-list") });
+	console.log("How are we doing now?");
 	/*
 	console.log("count: ", group.persons().length);
 	console.log(group);
