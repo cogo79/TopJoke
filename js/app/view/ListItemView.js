6d34867e186b96516f2f52216b21850f5baf9019
cogo79 <migobigo76@gmail.com>
2015-01-17 15:32:50 +0100

    For got to add new file ListItemView.js on previous commit.

---
+++ js/app/view/ListItemView.js
@@ -0,0 +1,21 @@
+define([], function(){
+	console.log("ListItemView.js");
+
+	return Backbone.View.extend({
+		template: _.template($("#li_tag_ME").html()),
+		text: '',
+		initialize: function(){
+			this.text = options.text;
+			this.render();
+		},
+		render: function(){
+			var variables = { text: this.text };
+    		var html = this.template(variables);
+			$(this.el).append(html);
+			
+			require(['app/controller'], function(controller) {
+				controller.update();
+			});
+		}
+	});
+});
